6d438a7ae93c00320dc88f096a1668ec07cd91d3
yurkimus <yurkimus@gmail.com>
2024-11-27 01:34:01 +0100

    Update dependencies

--- source/index.js
+++ source/index.js
@@ -4,11 +4,11 @@ import { is } from '@yurkimus/types'
 /**
  * Parses a cookie string into a list of key-value pairs.
  */
-export var parse = value => {
-  if (!is('String', value))
-    throw new TypeError(`Parameter 'value' must be a string.`)
+export let parse = source => {
+  if (!is('String', source))
+    throw new TypeError(`Parameter 'source' must be a string.`)
 
-  return value
+  return source
     .split('; ')
     .map(string => string.split('='))
     .reduce((object, [key, value]) => (object[key] = value, object), {})
@@ -17,14 +17,12 @@ export var parse = value => {
 /**
  * Reads cookies from a cookie string using a provided list of keys.
  */
-export var read = curry(
-  (key, value) => {
-    if (!is('String', value))
-      throw new TypeError(`Parameter 'value' must be a string.`)
+export let read = curry((key, source) => {
+  if (!is('String', source))
+    throw new TypeError(`Parameter 'source' must be a string.`)
 
-    return parse(value)[key]
-  },
-)
+  return parse(source)[key]
+})
 
 /**
  * Serializes a cookie from its components into a string.
@@ -34,13 +32,12 @@ export var read = curry(
  * let  name = 'name',
  *      value = 'John',
  *      attributes = [['path', '/'], ['expires', 'Wed, 21 Oct 2026 07:28:00 GMT']],
- *      serialized = serializeCookie(name, value, attributes); // => 'name=John; path=/; expires=Wed, 21 Oct 2026 07:28:00 GMT'
+ *      serialized = serialize(name, value, attributes); // => 'name=John; path=/; expires=Wed, 21 Oct 2026 07:28:00 GMT'
  * ```
  */
-export var serialize = curry(
-  (name, value, attributes) =>
-    name
-    + '='
-    + value
-    + attributes.reduce((string, attribute) => string + '; ' + attribute.join('='), ''),
+export let serialize = curry((name, value, attributes) =>
+  name
+  + '='
+  + value
+  + attributes.reduce((string, attribute) => string + '; ' + attribute.join('='), '')
 )
